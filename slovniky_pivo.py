c6b37d38c0d6ec4c8c31645971db240517a1b6d9
KatkaKlikova <145653984+KatkaKlikova@users.noreply.github.com>
2023-10-18 08:26:48 +0000

    doplneni

---
+++ slovniky_pivo.py
@@ -0,0 +1,18 @@
+# ------------------------------- TASK 06-02 -----------------------------------
+# Mějme piva na skladu nejmenované pivotéky uložené jako seznam slovníků:
+beers = [
+    { "name": "Brněnská 11", "brewery": "Parní pivovar Hauskrecht", "type": "Pale Lager", "IBU": 38, "ABV": 4.5, "count": 25 },
+    { "name": "Šlágr", "brewery": "Falkon", "type": "Pale Lager", "IBU": 35, "ABV": 4.0, "count": 13 },
+    { "name": "Hofburg", "brewery": "Topolský pivovar", "type": "Dark Lager", "IBU": 30, "ABV": 4.9, "count": 0 },
+    { "name": "Drakobití", "brewery": "Mazák", "type": "APA", "IBU": None, "ABV": None, "count": 8 },
+    { "name": "Single Hop Ale Eclipse", "brewery": "MadCat", "type": "APA", "IBU": None, "ABV": 5.0, "count": 0 },
+    { "name": "West Is The Best", "brewery": "MadCat", "type": "IPA", "IBU": None, "ABV": 6.3, "count": 17 },
+    { "name": "Philadelphia Sour Višeň", "brewery": "Obora", "type": "Sour Ale", "IBU": 15, "ABV": 4.7, "count": 19 },
+    { "name": "Franklin", "brewery": "Ládví Cobolis", "type": "NEIPA", "IBU": None, "ABV": 6.0, "count": 2 },
+]
+
+# Které pivo má nejvíc alkoholu (tj. má nejvyšší ABV)?
+
+# Která piva jsou vyprodaná (tj. count se rovná nule) - vytvořte seznam vyprodaných piv?
+
+# Zákazník si koupil 2x West Is The Best, zaznamenej tuto informaci do tabulky.
